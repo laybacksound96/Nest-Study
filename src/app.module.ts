47d97f25ffed543930212facebc4e23e3e3607ce
412 <laybacksound96@gmail.com>
2023-03-06 16:47:15 +0900

    환경변수 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
     MongooseModule.forRoot(
-      `mongodb+srv://laybacksound:zYgHQHorPWlseLej@cluster0.rtqo185.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://laybacksound:${process.env.DB_PASSWORD}@cluster0.rtqo185.mongodb.net/?retryWrites=true&w=majority`,
     ),
     UserModule,
   ],
