080411f45bb4d87dffd57276164d27eac4a0dd4a
412 <laybacksound96@gmail.com>
2023-03-07 11:01:22 +0900

    스키마 수정

--- src/user/schema/Gate.schema.ts
+++ src/user/schema/Gate.schema.ts
@@ -1,13 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Model } from 'mongoose';
 export type GateDocument = HydratedDocument<Gate>;
 @Schema()
 export class Gate {
-  @Prop()
+  @Prop({ type: Number, unique: true })
   gateIndex: number;
   @Prop({ required: true, default: false })
   isHardDifficulty: boolean;
   @Prop({ required: true, default: false })
   isChecked: boolean;
 }
-export const GateSchema = SchemaFactory.createForClass(Gate);
+const GateSchema = SchemaFactory.createForClass(Gate);
+
+export { GateSchema };
